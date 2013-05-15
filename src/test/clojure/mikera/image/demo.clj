0cfb1a06785e5d954493401d63b58dd4402059b6
mikera <mike.r.anderson.13@gmail.com>
2013-05-16 07:35:04 +0800

    demo edits

--- src/test/clojure/mikera/image/demo.clj
+++ src/test/clojure/mikera/image/demo.clj
@@ -11,9 +11,10 @@
 
 ;; demo of various filters
 (show (filter-image (grayscale) ant))
-(show (filter-image (box-blur 3 3) ant))
+(show (filter-image (box-blur 2 2) ant))
 (show (filter-image (contrast 0.5) ant))
 (show (filter-image (brightness 2.0) ant))
+(show (filter-image (invert) ant))
 
 )
 
