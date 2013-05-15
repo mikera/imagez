98f89ff95099df922e664544b8ee2dfa7829cad3
mikera <mike.r.anderson.13@gmail.com>
2013-05-15 17:50:53 +0800

    stup compoenents functions

--- src/main/clojure/mikera/image/colours.clj
+++ src/main/clojure/mikera/image/colours.clj
@@ -1,4 +1,5 @@
 (ns mikera.image.colours
+  (:use [mikera.cljutils error])
   (:import [mikera.image Colours]))
 
 (defn rgb ^long [r g b]
@@ -6,3 +7,13 @@
 
 (defn argb ^long [a r g b]
   (bit-and 0xFFFFFFFF (Colours/getARGBClamped (double a)  (double r) (double g) (double b))))
+
+(defn components-argb 
+  "Return the ARGB components of a colour value, in a 4-element vector of double values"
+  ([^long argb]
+    (TODO)))
+
+(defn components-rgb 
+  "Return the RGB components of a colour value, in a 3-element vector of double values"
+  ([^long argb]
+    (TODO)))
