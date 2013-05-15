abdd35cdf174e0a84103383365c7188012bcbc23
mikera <mike.r.anderson.13@gmail.com>
2013-05-15 16:52:14 +0800

    test scale-image

--- src/test/clojure/mikera/image/test_core.clj
+++ src/test/clojure/mikera/image/test_core.clj
@@ -6,3 +6,10 @@
 (deftest test-new-image
   (is (instance? BufferedImage (new-image 10 10))))
 
+(deftest test-scale-image
+  (let [^BufferedImage bi (new-image 10 10)
+        bi (scale-image bi 5 6)]
+    (is (instance? BufferedImage bi))
+    (is (== 5 (.getWidth bi)))
+    (is (== 6 (.getHeight bi)))))
+
