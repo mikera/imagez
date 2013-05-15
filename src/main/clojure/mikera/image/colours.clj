5b1778af6443a4cecffed35ff4f1e268ab36d1ba
mikera <mike.r.anderson.13@gmail.com>
2013-05-15 17:58:17 +0800

    Java colours and long-colour macro

--- src/main/clojure/mikera/image/colours.clj
+++ src/main/clojure/mikera/image/colours.clj
@@ -1,12 +1,18 @@
 (ns mikera.image.colours
   (:use [mikera.cljutils error])
-  (:import [mikera.image Colours]))
+  (:import [mikera.image Colours])
+  (:import [java.awt Color]))
+
+(defmacro long-colour 
+  "Macro to convert an integer ARGB value to a long colour value."
+  ([x]
+  `(bit-and 0xFFFFFFFF ~x)))
 
 (defn rgb ^long [r g b]
-  (bit-and 0xFFFFFFFF (Colours/getRGBClamped (double r) (double g) (double b))))
+  (long-colour (Colours/getRGBClamped (double r) (double g) (double b))))
 
 (defn argb ^long [a r g b]
-  (bit-and 0xFFFFFFFF (Colours/getARGBClamped (double a)  (double r) (double g) (double b))))
+  (long-colour (Colours/getARGBClamped (double a)  (double r) (double g) (double b))))
 
 (defn components-argb 
   "Return the ARGB components of a colour value, in a 4-element vector of double values"
@@ -17,3 +23,10 @@
   "Return the RGB components of a colour value, in a 3-element vector of double values"
   ([^long argb]
     (TODO)))
+
+(def JAVA-COLOURS
+  '(black blue cyan darkGray gray green lightGray magenta 
+    orange pink red white yellow))
+
+(doseq [colour JAVA-COLOURS]
+  (eval `(def ~colour (long-colour (.getRGB (. Color ~colour))))))
