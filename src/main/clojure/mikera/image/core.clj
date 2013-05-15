c1823b9e61397c3007fb0cdfccebbf58d7eb7ead
mikera <mike.r.anderson.13@gmail.com>
2013-05-15 15:39:33 +0800

    new-image function

--- src/main/clojure/mikera/image/core.clj
+++ src/main/clojure/mikera/image/core.clj
@@ -1,2 +1,11 @@
-(ns mikera.image.core)
+(ns mikera.image.core
+  (:import [java.awt.image BufferedImage]))
 
+(set! *unchecked-math* true)
+(set! *warn-on-reflection* true)
+
+(defn new-image
+  "Creates a new BufferedImage with the specified width and height. 
+   Uses ARGB format by default."
+  [^long width ^long height]
+  (BufferedImage. (int width) (int height) BufferedImage/TYPE_INT_ARGB))
