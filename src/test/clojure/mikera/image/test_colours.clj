86523eb4a943c2de4274a8fe35590d3100a40102
guns <self@sungpae.com>
2013-07-31 05:01:57 -0500

    Add components-a?rgb with tests

--- src/test/clojure/mikera/image/test_colours.clj
+++ src/test/clojure/mikera/image/test_colours.clj
@@ -5,6 +5,12 @@
 (deftest test-rgb
   (is (== 0xFF000000 (rgb 0 0 0))))
 
+(deftest test-components-argb
+  (is (= [0xDE 0xAD 0xBE 0xEF] (components-argb 0xDEADBEEF))))
+
+(deftest test-components-rgb
+  (is (= [0xFE 0xBA 0xBE] (components-rgb 0xCAFEBABE))))
+
 (deftest test-rand-colour
   (let [rc (rand-colour)]
     (is (== 0xFF000000 (bit-and 0xFF000000 rc)))))
