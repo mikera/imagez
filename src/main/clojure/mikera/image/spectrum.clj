aeea37ee11d2a9edabeb7fbe72b4e8a03084cdc6
mikera <mike.r.anderson.13@gmail.com>
2013-12-22 01:04:46 +0000

    add rainbow wheel spectrum

--- src/main/clojure/mikera/image/spectrum.clj
+++ src/main/clojure/mikera/image/spectrum.clj
@@ -3,6 +3,13 @@
   (:import [mikera.util Maths])
   (:import [java.awt.image BufferedImage]))
 
+;; set of spectrum generation functions
+;;
+;; each of these maps a double value to an ARGB colour
+;;
+;; colours are generally viewable in the range 0..1
+;; though the spectrums can continue / be generated outside this range
+
 (defn heatmap
   (^long [^double x]
     (let [x (* 8 (- x 0.5))
@@ -17,4 +24,17 @@
           r (Maths/logistic x)
           g r
           b r]
+      (rgb r g b))))
+
+(defn wheel
+  (^long [^double x]
+    (let [x (- x (java.lang.Math/floor x))
+          x (* x Maths/TAU)
+          t (* 0.333333333 Maths/TAU)
+          r (Math/cos x)
+          g (Math/cos (- x t))
+          b (Math/cos (+ x t))
+          r (+ 0.5 (* 0.5 r))
+          g (+ 0.5 (* 0.5 g))
+          b (+ 0.5 (* 0.5 b))]
       (rgb r g b))))
