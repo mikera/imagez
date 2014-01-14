36888f14e7a9cb78338d0f2610dfe4eb33c58137
mikera <mike.r.anderson.13@gmail.com>
2014-01-14 21:59:34 +0800

    tidy and document demo

--- src/test/clojure/mikera/image/demo.clj
+++ src/test/clojure/mikera/image/demo.clj
@@ -5,20 +5,21 @@
   (:use mikera.image.spectrum)
   (:require [clojure.java.io :refer [resource]]))
 
+;; load an image from a packaged resouce on the classpath
 (def ant (-> "mikera/image/samples/Ant.png" resource load-image))
 
 (defn demo []
-
-(show ant)
-
-;; demo of various filters used in functional style
-(show ((grayscale) ant))
-(show ((box-blur 2 2) ant))
-(show ((contrast 0.5) ant))
-(show ((brightness 2.0) ant))
-(show ((invert) ant))
-
-;; demo of visualising a colour gradient
-(show (gradient-image wheel))
-
+	
+	 ;; show a basic image
+	(show ant)
+	
+	;; demo of various filters used in functional style
+	(show ((grayscale) ant))
+	(show ((box-blur 2 2) ant))
+	(show ((contrast 0.5) ant))
+	(show ((brightness 2.0) ant))
+	(show ((invert) ant))
+	
+	;; demo of visualising a colour gradient
+	(show (gradient-image wheel))
 )
