1d86656920d4df99fd8044f1e8b598251ee513ca
Scott Douglas-Watson <scottdw0001@gmail.com>
2014-10-31 12:34:56 +0000

    Remove the ability to create a image without alpha in new-image.

--- src/main/clojure/mikera/image/core.clj
+++ src/main/clojure/mikera/image/core.clj
@@ -18,11 +18,7 @@
   "Creates a new BufferedImage with the specified width and height.
    Uses ARGB format by default."
   (^BufferedImage [width height]
-    (new-image width height true))
-  (^BufferedImage [width height alpha?]
-    (if alpha?
-      (BufferedImage. (int width) (int height) BufferedImage/TYPE_INT_ARGB)
-      (BufferedImage. (int width) (int height) BufferedImage/TYPE_INT_RGB))))
+    (BufferedImage. (int width) (int height) BufferedImage/TYPE_INT_ARGB)))
 
 (defn resize
   "Resizes an image to the specified width and height. If height is omitted,
--- src/test/clojure/mikera/image/test_core.clj
+++ src/test/clojure/mikera/image/test_core.clj
@@ -13,9 +13,7 @@
 
 (deftest test-new-image
   (is (instance? BufferedImage (new-image 10 10)))
-  (is (instance? BufferedImage (new-image 10 10 false)))
-  (is (= BufferedImage/TYPE_INT_ARGB (.getType (new-image 10 10))))
-  (is (= BufferedImage/TYPE_INT_RGB (.getType (new-image 10 10 false)))))
+  (is (= BufferedImage/TYPE_INT_ARGB (.getType (new-image 10 10)))))
 
 (deftest test-scale-image
   (let [^BufferedImage bi (new-image 10 10)
