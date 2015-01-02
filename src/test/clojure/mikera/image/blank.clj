ea1cad133093e4a462cab1a28daf4bf324980abb
mikera <mike.r.anderson.13@gmail.com>
2015-01-02 10:09:55 +0800

    extra test namespaces

---
+++ src/test/clojure/mikera/image/blank.clj
@@ -0,0 +1,1 @@
+(ns mikera.image.blank)
---
+++ src/test/clojure/mikera/image/test_io.clj
@@ -0,0 +1,37 @@
+(ns mikera.image.test-io
+  (:use mikera.image.core)
+  (:use clojure.test)
+  (:require [clojure.java.io :refer [as-file resource input-stream]])
+  (:import javax.imageio.ImageIO
+           javax.imageio.ImageWriter
+           javax.imageio.ImageWriteParam))
+
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* true)
+
+(deftest test-load-image
+  (testing "image loading"
+    (are [r] (instance? java.awt.image.BufferedImage (load-image r))
+         "src/test/resources/mikera/image/samples/Clojure_300x300.png"
+         (as-file "src/test/resources/mikera/image/samples/Clojure_300x300.png")
+         (ImageIO/read (as-file "src/test/resources/mikera/image/samples/Clojure_300x300.png"))
+         (resource "mikera/image/samples/Clojure_300x300.png")
+         (input-stream "src/test/resources/mikera/image/samples/Clojure_300x300.png")))
+  (testing "load image type conversion"
+    (is (= java.awt.image.BufferedImage/TYPE_INT_ARGB (.getType (load-image "src/test/resources/mikera/image/samples/Clojure_300x300.png"))))))
+
+(deftest test-compression
+  (testing "png"
+    (let [^ImageWriter writer (.next (ImageIO/getImageWritersByFormatName "png"))
+          param (.getDefaultWriteParam writer)]
+      ;; setting compression quality is not actually supported for PNG, this test
+      ;; is just validates that it doesn't crash
+      (is (= param (#'mikera.image.core/apply-compression param 1.0)))))
+
+  (testing "jpeg"
+    (let [^ImageWriter writer (.next (ImageIO/getImageWritersByFormatName "jpeg"))
+          param (.getDefaultWriteParam writer)
+          compression-fn #'mikera.image.core/apply-compression]
+      (is (= param (#'mikera.image.core/apply-compression param 1.0)))
+      (is (= (.getCompressionQuality ^ImageWriteParam (compression-fn param 1.0)) 1.0))
+      (is (= (.getCompressionQuality ^ImageWriteParam (compression-fn param 0.5)) 0.5)))))
