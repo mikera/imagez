7cfc22d35d0f727effc446a1b9c1375803036c13
Mike <mike@mikera.net>
2015-06-29 11:43:43 +0100

    update for project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject imagez "0.5.1-SNAPSHOT"
+(defproject imagez "0.6.1-SNAPSHOT"
   :description "Image processing library for Clojure"
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [net.mikera/cljunit "0.3.1"]
-                 [net.mikera/clojure-utils "0.6.1"]
+                 [net.mikera/clojure-utils "0.6.2"]
                  [org.imgscalr/imgscalr-lib "4.2"]
                  [net.mikera/mathz "0.3.0"]
                  [net.mikera/mikera-gui "0.3.1"]
